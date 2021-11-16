f5a62f09cb8160fc0fe7c44fcf491c0bca2dc469
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2021-11-16 13:05:59 +0700

    Initial Commit

---
+++ xcode_triangle_glfw/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcode_triangle_glfw
+//
+//  Created by Rizky Darmawan on 16/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
